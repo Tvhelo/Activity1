b217764dd65a9f5024911fc7a08c6458bbfc13d6
Octavio Hdz <A01739304@tec.mx>
2026-04-17 23:56:24 -0600

    parte 3

--- lib/automata.ex
+++ lib/automata.ex
@@ -59,6 +59,17 @@ defmodule Automata do
     e_closure_walk(delta, start, start)
   end
 
+  def e_determinize({_q, sigma, _delta, q0, f} = nfae) do
+    q0_prime = e_closure(nfae, [q0])
+
+    {q_prime, d_prime} =
+      e_determinize_reachable(nfae, sigma, [q0_prime], [], %{})
+
+    f_prime = Enum.filter(q_prime, fn r -> intersects?(r, f) end)
+
+    {q_prime, sigma, d_prime, q0_prime, f_prime}
+  end
+
   def powerset([]), do: [[]]
 
   def powerset([h | t]) do
@@ -111,5 +122,51 @@ defmodule Automata do
       end)
 
     e_closure_walk(delta, rest2, visited2)
+  end
+
+  defp e_determinize_reachable(_nfae, _sigma, [], visited, d_prime) do
+    {Enum.sort(visited), d_prime}
+  end
+
+  defp e_determinize_reachable(nfae, sigma, [r | open], visited, d_prime) do
+    if r in visited do
+      e_determinize_reachable(nfae, sigma, open, visited, d_prime)
+    else
+      {new_transitions, new_states} = process_symbols(nfae, r, sigma, %{}, [])
+
+      e_determinize_reachable(
+        nfae,
+        sigma,
+        open ++ new_states,
+        [r | visited],
+        Map.merge(d_prime, new_transitions)
+      )
+    end
+  end
+
+  defp process_symbols(_nfae, _r, [], transitions, new_states) do
+    {transitions, new_states}
+  end
+
+  defp process_symbols(nfae, r, [a | rest], transitions, new_states) do
+    s = move_with_epsilon(nfae, r, a)
+
+    if s == [] do
+      process_symbols(nfae, r, rest, transitions, new_states)
+    else
+      process_symbols(
+        nfae,
+        r,
+        rest,
+        Map.put(transitions, {r, a}, s),
+        new_states ++ [s]
+      )
+    end
+  end
+
+  defp move_with_epsilon({_q, _sigma, delta, _q0, _f} = nfae, r, a) do
+    closure_r = e_closure(nfae, r)
+    moved = move(delta, closure_r, a)
+    e_closure(nfae, moved)
   end
 end
--- test/automata_test.exs
+++ test/automata_test.exs
@@ -49,4 +49,33 @@ defmodule AutomataTest do
 
     assert Automata.e_closure(nfae, [4, 8]) == [1, 2, 3, 4, 6, 7, 8]
   end
+
+  test "e_determinize builds a DFA from epsilon NFA" do
+    {q_prime, sigma, d_prime, q0_prime, f_prime} =
+      Automata.part3_nfa_epsilon()
+      |> Automata.e_determinize()
+
+    s0 = [0, 1, 2, 3, 7]
+    s1 = [1, 2, 3, 4, 6, 7, 8]
+    s2 = [1, 2, 3, 5, 6, 7]
+    s3 = [1, 2, 3, 5, 6, 7, 9]
+    s4 = [1, 2, 3, 5, 6, 7, 10]
+
+    assert sigma == [:a, :b]
+    assert q0_prime == s0
+
+    assert s0 in q_prime
+    assert s1 in q_prime
+    assert s2 in q_prime
+    assert s3 in q_prime
+    assert s4 in q_prime
+
+    assert d_prime[{s0, :a}] == s1
+    assert d_prime[{s0, :b}] == s2
+    assert d_prime[{s1, :a}] == s1
+    assert d_prime[{s1, :b}] == s3
+    assert d_prime[{s3, :b}] == s4
+
+    assert s4 in f_prime
+  end
 end
